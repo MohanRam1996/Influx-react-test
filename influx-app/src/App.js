66c079b5baf19c83c5cd680a818aaddab2ca9a19
mohanram <mohanram.shanmugam96@gmail.com>
2021-05-22 11:10:28 +0530

    Initial Commit

---
+++ influx-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+
+
+class App extends React.Component{
+render(){
+    return (
+      <React.Fragment>
+        test
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
